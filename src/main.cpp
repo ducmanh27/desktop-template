e367b0e2da8f8e8d614885d6c631416aa9835fc4
Phan Duc Manh <phanmanhk57a1@gmail.com>
2025-01-11 20:43:29 +0700

    chore: change using system lib spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <QCoreApplication>
 
-// Add spdlog lib
-// Add KDE Framework
-// Add google test
-
 #include <QDebug>
 #include <ThreadWeaver/ThreadWeaver>
 #include "spdlog/spdlog.h"
+#include <QTimer>
+using namespace ThreadWeaver;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -14,16 +12,16 @@ int main(int argc, char *argv[])
     // Set up code that uses the Qt event loop here.
     // Call a.quit() or a.exit() to quit the application.
     // A not very useful example would be including
-    // #include <QTimer>
+
     // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
+    QTimer::singleShot(5000, &a, &QCoreApplication::quit);
     // which quits the application after 5 seconds.
 
     // If you do not need a running Qt event loop, remove the call
     // to a.exec() or use the Non-Qt Plain C++ Application template.
-    using namespace ThreadWeaver;
+
     stream() << make_job([]() {
-        spdlog::info("hello world");
+        spdlog::info("[{} {}] hello world ",__FILE__, __LINE__);
     });
     return a.exec();
 }
